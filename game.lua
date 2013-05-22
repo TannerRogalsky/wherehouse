1295f7d3807299ac05c7312680b7f8da445c06bb
Tanner Rogalsky <the.t.dawg@gmail.com>
2013-05-21 22:50:54 -0400

    added mouse_moved callback to game

--- game.lua
+++ game.lua
@@ -7,6 +7,7 @@ function Game:initialize()
     -- the anonymous function wrapping the call to the actual function is a stupid hack that totally works
     beholder.observe("mouse_down", function(x, y, button) self:mouse_down(x, y, button) end)
     beholder.observe("mouse_up", function(x, y, button) self:mouse_up(x, y, button) end)
+    beholder.observe("mouse_moved", function(x, y) self:mouse_moved(x, y) end)
     beholder.observe("key_down", function(key, unicode) self:key_down(key, unicode) end)
     beholder.observe("key_up", function(key, unicode) self:key_up(key, unicode) end)
   end)
@@ -18,6 +19,10 @@ end
 
 function Game:mouse_up(x, y, button)
   print(x, y, button)
+end
+
+function Game:mouse_moved(x, y)
+  print(x, y)
 end
 
 function Game:key_down(key, unicode)
